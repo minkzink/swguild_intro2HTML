55c99d1aaa5ff1a5dda38a51452ea77a2735f22d
minkzink <ben@minkzink.com>
2017-03-29 10:45:41 -0400

    Auditon Edits v2

--- Audition/script.js
+++ Audition/script.js
@@ -18,9 +18,9 @@ function resetForm() {
     document.getElementById("results").style.display = "none";
     document.getElementById("submitButton").innerText = "Submit";
     document.forms["newForm"]["num1"].focus();
-}
+};
 function validateItems() {
-    clearErrors();
+    resetForm();
     var num1 = document.forms["newForm"]["num1"].value;
     var num2 = document.forms["newForm"]["num2"].value;
     var num3 = document.forms["newForm"]["num3"].value;
@@ -30,39 +30,38 @@ function validateItems() {
            .parentElement.className = "form-group has-error";
         document.forms["newForm"]["num1"].focus();
         return false;
-    }
+    };
    if (num2 <= 0 || isNaN(num2)) {
        alert("Num2 must be filled in with a number.");
        document.forms["newForm"]["num2"]
           .parentElement.className = "form-group has-error"
        document.forms["newForm"]["num2"].focus();
        return false;
-   }
+   };
    if (num3 <= 0 || isNaN(num3)) {
        alert("Num3 must be filled in with a number.");
        document.forms["newForm"]["num3"]
           .parentElement.className = "form-group has-error"
        document.forms["newForm"]["num3"].focus();
        return false;
-   }
-   /*if (num2 >= num1) {
+   };
+   if (num2 <= num1) {
        alert("Num2 must be greater than Num1.");
        document.forms["newForm"]["num2"]
           .parentElement.className = "form-group has-error"
        document.forms["newForm"]["num2"].focus();
        return false;
-   }*/
-   function evensFoundText ( num1, num2, num3 ) {
+   };
+   function Number( num1, num2, num3 ) {
      for(var i = num1; i < num2; i+= num3){
            if(i % 2 === 0){
               console.log(i);
-              return false;
         }
      }
    }
    document.getElementById("results").style.display = "block";
    document.getElementById("submitButton").innerText = "Recalculate";
-   document.getElementById("evensFound").innerText = "Sample Text"
+   document.getElementById("Number").innerText = "give me some text";
    // We are returning false so that the form doesn't submit
    // and so that we can see the results
    return false;
